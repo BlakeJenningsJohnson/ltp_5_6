0ff384ddffe915c86435224e7d3a53230768b4da
BlakeJenningsJohnson <blake.jennings@gmail.com>
2013-10-29 21:30:54 -0700

    Update greeting.rb

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+ puts 'Hello there, and what\'s your first name?'
+ first_name = gets.chomp + ' '
+ puts 'And your middle name?'
+ middle_name = gets.chomp + ' '
+ puts 'And your last name?'
+ last_name = gets.chomp
+ puts 'Nice to meet you, ' + first_name + middle_name + last_name + '!'
+
+
+
+
+
+
